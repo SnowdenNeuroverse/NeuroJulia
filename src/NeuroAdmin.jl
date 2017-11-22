aad643ce165f49187aa621e6075016a3f34cfe3a
SnowdenNeuroverse <33819384+SnowdenNeuroverse@users.noreply.github.com>
2017-11-22 11:48:52 +0800

    Update NeuroAdmin.jl

--- src/NeuroAdmin.jl
+++ src/NeuroAdmin.jl
@@ -1,34 +1,5 @@
 module NeuroAdmin
-    using JSON
-    using Requests
     using DataFrames
-
-    global token = ENV["JUPYTER_TOKEN"]
-    global domain = ENV["NV_DOMAIN"] * ":8082/NeuroApi/"
-
-    function neurocall(method_address,requestbody)
-        url = domain * method_address
-        msgdata = nothing
-        msgdatalength = 0
-        if requestbody!=nothing
-            msgdata = JSON.json(requestbody)
-            msgdatalength = length(msgdata)
-        end
-        headers = Dict("Content-Length" => string(msgdatalength), "Token" => token)
-        response = post(url; headers=headers, data=msgdata)
-        if response.status != 200
-            if response.status == 401
-                error("Session has expired: Log into Neuroverse and connect to your Notebooks session or reload the Notebooks page in Neuroverse")
-            else
-                error("Neuroverse connection error: Http code " * string(response.status))
-            end
-        end
-        responseobj = JSON.parse(readstring(response))
-        if responseobj["Error"] != nothing
-            error("Neuroverse Error: " * responseobj["Error"])
-        end
-        return responseobj
-    end
 
     function getactivesessions()
         method_address="notebookmanagementservice/api/notebookmanagement/getdetailedsessionlist/"
