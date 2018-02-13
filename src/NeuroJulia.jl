8ee356aaec77ef475a03cfa27a1cad8997c0e0e6
SnowdenNeuroverse <33819384+SnowdenNeuroverse@users.noreply.github.com>
2018-02-13 10:35:38 +0800

    Update Dev Url

--- src/NeuroJulia.jl
+++ src/NeuroJulia.jl
@@ -14,7 +14,7 @@ module NeuroJulia
     elseif contains(domain,"sit")
         "https://neurosit.snowdenonline.com.au"
     else
-        "http://dev-stratos.australiaeast.cloudapp.azure.com"
+        "http://neurodev.snowdenonline.com.au"
     end
     global homedir = "/home/jovyan/session/"
 
