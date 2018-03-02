fae3e710e61dadb75a0c2bd8d8804fd0c27ead8d
SnowdenNeuroverse <33819384+SnowdenNeuroverse@users.noreply.github.com>
2018-03-02 08:29:48 +0800

    Update NeuroJulia.jl

--- src/NeuroJulia.jl
+++ src/NeuroJulia.jl
@@ -27,7 +27,7 @@ module NeuroJulia
             msgdatalength = length(msgdata)
         end
         headers = Dict("Content-Length" => string(msgdatalength), "Token" => token, "Accept" => "application/json", "Content-Type" => "application/json")
-        response = post(url; headers=headers, data=msgdata, timeout=timout)
+        response = post(url; headers=headers, data=msgdata, timeout=timeout)
         if response.status != 200
             if response.status == 401
                 error("Session has expired: Log into Neuroverse and connect to your Notebooks session or reload the Notebooks page in Neuroverse")
