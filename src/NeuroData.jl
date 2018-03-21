028d1b92dded0e00329615fd3c1f1ecf2459104a
Lee Bates <batesl87@gmail.com>
2018-03-21 09:53:53 +0800

    Update NeuroData.jl

--- src/NeuroData.jl
+++ src/NeuroData.jl
@@ -237,7 +237,7 @@ module NeuroData
     function add_destination_table_indexes(;tablename=nothing,tableindexes::Array{DestinationTableDefinitionIndex,1}=nothing)
         request=GetDestinationTableDefinitionRequest(tablename)
         table_def=NeuroJulia.neurocall("DataPopulation","GetDestinationTableDefinition",request)["DestinationTableDefinitions"][1]
-        append!(table_def["DestinationTableDefinitions"][1]["DestinationTableDefinitionIndexes"],JSON.parse(JSON.json(tableindexes)))
+        append!(table_def["DestinationTableDefinitionIndexes"],JSON.parse(JSON.json(tableindexes)))
         NeuroJulia.neurocall("datapopulation","UpdateDestinationTableDefinition",table_def)
     end
 
