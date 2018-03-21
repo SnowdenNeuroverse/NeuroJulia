c7c39374428ff7d3cfafb4e205097d2b6d9e8710
Lee Bates <batesl87@gmail.com>
2018-03-21 09:08:05 +0800

    Update NeuroData.jl

--- src/NeuroData.jl
+++ src/NeuroData.jl
@@ -234,6 +234,12 @@ module NeuroData
         return new_table_def
     end
 
+    function add_destination_table_indexes(tablename=nothing,tableindexes::Array{DestinationTableDefinitionIndex,1}=nothing)
+        table_def=get_table_definition(tablename=tablename)
+        append!(table_def.DestinationTableDefinitionIndexes,tableindexes)
+        table_def
+    end
+
     function save_table_definition(;tabledef=nothing,filename=nothing)
         def=JSON.json(tabledef)
         file=open(filename,"w+")
