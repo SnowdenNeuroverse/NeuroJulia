904e86b6527ddf1cc72a51f8ac1cf071273f9e6b
SnowdenLeeBates <39359831+SnowdenLeeBates@users.noreply.github.com>
2018-06-15 15:21:50 +0800

    Update NeuroDataTypes.jl

--- src/NeuroData/NeuroDataTypes.jl
+++ src/NeuroData/NeuroDataTypes.jl
@@ -1,3 +1,8 @@
+abstract type AbstractSourceParameters end
+abstract type AbstractSinkParameters end
+abstract type AbstractStreamRequest end
+abstract type AbstractStreamResponse end
+
 function getdatatypes()
     return ["Boolean","Int32","Int64","Decimal","Double","DateTime","Guid","String"]
 end
