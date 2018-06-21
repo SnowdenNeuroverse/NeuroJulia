0f7ac5df37b9df1bb37b46d78b11265148644bc0
SnowdenLeeBates <39359831+SnowdenLeeBates@users.noreply.github.com>
2018-06-21 15:10:38 +0800

    Update NeuroDataDatalakeCommands.jl

--- src/NeuroData/NeuroDataDatalakeCommands.jl
+++ src/NeuroData/NeuroDataDatalakeCommands.jl
@@ -4,8 +4,11 @@ type DataLakeDeleteFileRequest
     TableName::String
     FilePath::String
 end
-function deletedatalakefile!(datastorename::String,tablename::String,filepath::String)
-    request=DataLakeDeleteFileRequest(datastorename,tablename,filepath)
+function deletedatalakefile!(datastorename::String,tablename::String,filename_including_partition::String)
+    table_def=get_table_definition(storename=datastorename,tablename=tablename)
+    schematype=filter(tuple->last(tuple)==table_def.SchemaType,collect(schema_type_map))[1][1]
+    folderpath=lowercase("/managed/$schematype/table/$tablename/")
+    request=DataLakeDeleteFileRequest(datastorename,tablename,folderpath*strip(filename_including_partition,'/'))
     NeuroJulia.neurocall("8080","DataMovementService","DataLakeDeleteFile",request)
     return nothing
 end
@@ -15,7 +18,8 @@ type ListDataLakeTableFilesRequest
     TableName::String
 end
 
-function listdatalaketablefiles(datastorename::String,tablename::String)
+function listdatalaketablefileswithpartitions(datastorename::String,tablename::String)
     request=ListDataLakeTableFilesRequest(datastorename,tablename)
-    return NeuroJulia.neurocall("8080","DataMovementService","ListDataLakeTableFiles",request)["Files"]
+    files=NeuroJulia.neurocall("8080","DataMovementService","ListDataLakeTableFiles",request)["Files"]
+    return [split(files[i],tablename)[2] for i=1:length(files)]
 end
