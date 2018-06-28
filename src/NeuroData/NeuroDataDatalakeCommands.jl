e97ef4a0c5bbc69cc704522529cec4eb3615255f
SnowdenLeeBates <39359831+SnowdenLeeBates@users.noreply.github.com>
2018-06-28 11:46:32 +0800

    Update NeuroDataDatalakeCommands.jl

--- src/NeuroData/NeuroDataDatalakeCommands.jl
+++ src/NeuroData/NeuroDataDatalakeCommands.jl
@@ -52,8 +52,25 @@ function get_lines_in_datalake_csv(datastorename::String,tablename::String,filen
     schematype=filter(tuple->last(tuple)==table_def.SchemaType,collect(schema_type_map))[1][1]
     folderpath=lowercase("/managed/$schematype/table/$tablename/")
     request=GetLinesInDataLakeCsvFileRequest(datastorename,tablename,folderpath*strip(filename_including_partition,'/'))
-    lines=NeuroJulia.neurocall("8080","DataMovementService","GetLinesInDataLakeCsvFile",request)["Lines"]
-    return lines
+    response=NeuroJulia.neurocall("8080","DataMovementService","GetLinesInDataLakeCsvFile",request)
+    check_request=Dict("JobId"=>response["JobId"])
+    status=0
+    errormsg=""
+    while(status==0)
+        sleep(1)
+        response_c=NeuroJulia.neurocall("8080","DataMovementService","CheckJob",check_request)
+        status=response_c["Status"]
+        if status>1
+            errormsg=response_c["Message"]
+        end
+    end
+    
+    NeuroJulia.neurocall("8080","DataMovementService","FinaliseJob",check_request)
+
+    if status!=1
+        error("Neuroverse error: " * errormsg)
+    end
+    return parse(errormsg)
 end
 
 type DataLakeReChunkCsvFileRequest
