70ffb527d2b7bb6cdb7dfe16d37d83907af9f021
SnowdenLeeBates <39359831+SnowdenLeeBates@users.noreply.github.com>
2018-07-03 15:49:45 +0800

    Update NeuroDataDatalakeCommands.jl

--- src/NeuroData/NeuroDataDatalakeCommands.jl
+++ src/NeuroData/NeuroDataDatalakeCommands.jl
@@ -9,20 +9,20 @@ function delete_datalake_file!(datastorename::String,tablename::String,filename_
     schematype=filter(tuple->last(tuple)==table_def.SchemaType,collect(schema_type_map))[1][1]
     folderpath=lowercase("/managed/$schematype/table/$tablename/")
     request=DataLakeDeleteFileRequest(datastorename,tablename,folderpath*strip(filename_including_partition,'/'))
-    response=NeuroJulia.neurocall("8080","DataMovementService","DataLakeDeleteFile",request)
+    response=NeuroJulia.neurocall("80","DataMovementService","DataLakeDeleteFile",request)
     check_request=Dict("JobId"=>response["JobId"])
     status=0
     errormsg=""
     while(status==0)
         sleep(1)
-        response_c=NeuroJulia.neurocall("8080","DataMovementService","CheckJob",check_request)
+        response_c=NeuroJulia.neurocall("80","DataMovementService","CheckJob",check_request)
         status=response_c["Status"]
         if status>1
             errormsg=response_c["Message"]
         end
     end
     
-    NeuroJulia.neurocall("8080","DataMovementService","FinaliseJob",check_request)
+    NeuroJulia.neurocall("80","DataMovementService","FinaliseJob",check_request)
 
     if status!=1
         error("Neuroverse error: " * errormsg)
@@ -37,7 +37,7 @@ end
 
 function list_datalake_table_files_with_partitions(datastorename::String,tablename::String)
     request=ListDataLakeTableFilesRequest(datastorename,tablename)
-    files=NeuroJulia.neurocall("8080","DataMovementService","ListDataLakeTableFiles",request)["Files"]
+    files=NeuroJulia.neurocall("80","DataMovementService","ListDataLakeTableFiles",request)["Files"]
     return [split(files[i],lowercase(tablename))[2] for i=1:length(files)]
 end
 
@@ -52,20 +52,20 @@ function get_lines_in_datalake_csv(datastorename::String,tablename::String,filen
     schematype=filter(tuple->last(tuple)==table_def.SchemaType,collect(schema_type_map))[1][1]
     folderpath=lowercase("/managed/$schematype/table/$tablename/")
     request=GetLinesInDataLakeCsvFileRequest(datastorename,tablename,folderpath*strip(filename_including_partition,'/'))
-    response=NeuroJulia.neurocall("8080","DataMovementService","GetLinesInDataLakeCsvFile",request)
+    response=NeuroJulia.neurocall("80","DataMovementService","GetLinesInDataLakeCsvFile",request)
     check_request=Dict("JobId"=>response["JobId"])
     status=0
     errormsg=""
     while(status==0)
         sleep(1)
-        response_c=NeuroJulia.neurocall("8080","DataMovementService","CheckJob",check_request)
+        response_c=NeuroJulia.neurocall("80","DataMovementService","CheckJob",check_request)
         status=response_c["Status"]
         if status>=1
             errormsg=response_c["Message"]
         end
     end
     
-    NeuroJulia.neurocall("8080","DataMovementService","FinaliseJob",check_request)
+    NeuroJulia.neurocall("80","DataMovementService","FinaliseJob",check_request)
 
     if status!=1
         error("Neuroverse error: " * errormsg)
@@ -85,20 +85,20 @@ function rechunk_datalake_csv(datastorename::String,fromtablename::String,filena
     schematype=filter(tuple->last(tuple)==table_def.SchemaType,collect(schema_type_map))[1][1]
     folderpath=lowercase("/managed/$schematype/table/$fromtablename/")
     request=DataLakeReChunkCsvFileRequest(datastorename,fromtablename,folderpath*strip(filename_including_partition,'/'),totablename)
-    response=NeuroJulia.neurocall("8080","DataMovementService","DataLakeReChunkCsvFile",request)
+    response=NeuroJulia.neurocall("80","DataMovementService","DataLakeReChunkCsvFile",request)
     check_request=Dict("JobId"=>response["JobId"])
     status=0
     errormsg=""
     while(status==0)
         sleep(1)
-        response_c=NeuroJulia.neurocall("8080","DataMovementService","CheckJob",check_request)
+        response_c=NeuroJulia.neurocall("80","DataMovementService","CheckJob",check_request)
         status=response_c["Status"]
         if status>1
             errormsg=response_c["Message"]
         end
     end
     
-    NeuroJulia.neurocall("8080","DataMovementService","FinaliseJob",check_request)
+    NeuroJulia.neurocall("80","DataMovementService","FinaliseJob",check_request)
 
     if status!=1
         error("Neuroverse error: " * errormsg)
